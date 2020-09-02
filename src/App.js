bfbcc87267071062d2b2375713798a57f641d0ca
Harshvardhan <harshvardhanvipat@gmail.com>
2020-09-02 16:59:03 +1000

    restructuring of the application created new functional components

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   const [manualActive, changedManualActive] = useState(true);
-  const [direction, setDirection] = useState("increment");
+  // const [direction, setDirection] = useState("increment");
 
   const ICREMENT_SYMBOL = "+";
   const DECREMENT_SYMBOL = "-";
@@ -18,44 +18,6 @@ function App() {
 
   return (
     <div>
-      <div className="displayNumberDivHolder">
-        <DisplayNumber currentValue={count}></DisplayNumber>
-      </div>
-
-      <div className="ui grid equal  increment-decrement-button-holder-div">
-        <h2 className="ui center aligned icon header">
-          <i className="sign language icon"></i>
-          Manual
-        </h2>
-
-        <div className="equal width row">
-          <div className="increment column">
-            <button
-              onClick={
-                manualActive === true
-                  ? () => setCount((count) => count + 1)
-                  : classButton + "disabled"
-              }
-              className="ui secondary button"
-            >
-              {ICREMENT_SYMBOL}
-            </button>
-          </div>
-          <div className="decrement column">
-            <button
-              onClick={() =>
-                manualActive && count === 0
-                  ? undefined
-                  : setCount((count) => count - 1)
-              }
-              className="ui secondary button"
-            >
-              {DECREMENT_SYMBOL}
-            </button>
-          </div>
-        </div>
-      </div>
-
       <AutomaticDisplayComponent
         currentCount={count}
         setCount={setCount}
@@ -63,13 +25,7 @@ function App() {
         togglefunctionality={() => {
           changedManualActive((m) => !m);
         }}
-        direction={direction}
-        setDirection={setDirection}
       />
-
-      <div>{console.log(manualActive)}</div>
-      <FetchData displayNumberDetails={count} />
-      {/* {direction} */}
     </div>
   );
 }
--- src/components/AutomaticDisplayComponent.js
+++ src/components/AutomaticDisplayComponent.js
@@ -1,10 +1,132 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Dropdown } from "semantic-ui-react";
 import "../css/AutomaticDisplayComponent.css";
+import DisplayNumber from "./DisplayNumberComponent";
+import FetchData from "../apis/FetchData";
 
 function AutomaticDisplayComponent(props) {
-  const savedCallback = useRef();
-  return <div> test div </div>;
+  return <DisplayButtons />;
 }
 
+function DisplayButtons(props) {
+  const [direction, setDirection] = useState("Increment");
+  const [playPauseMode, setPlayPauseMode] = useState(false);
+
+  console.log(props);
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <div className="ui text container">
+        <h2 className="ui header">Header</h2>
+        {props.counter}
+        <button
+          onClick={() => {
+            setPlayPauseMode((m) => !m);
+          }}
+        >
+          Play/ Pause
+        </button>
+        <button onClick={() => setDirection("Increment")}> + </button>
+        <button onClick={() => setDirection("Decrement")}> - </button>
+        {direction}
+        {console.log(playPauseMode)}
+      </div>
+      <CounterDelaySimulator
+        playPauseMode={playPauseMode}
+        direction={direction}
+      />
+    </div>
+  );
+}
+
+function CounterDelaySimulator(props) {
+  const [delay, setDelay] = useState(1000);
+  const [counter, setCounter] = useState(0);
+
+  useInterval(() => {
+    if (props.playPauseMode) {
+      props.direction === "Increment"
+        ? setCounter(() => counter + 1)
+        : setCounter(counter - 1);
+    }
+  }, delay);
+
+  function handleDelayChange(e) {
+    setDelay(Number(e.target.value));
+  }
+
+  return (
+    <>
+      <div>{counter}</div>
+      <button
+        onClick={() => {
+          setDelay(500);
+        }}
+      >
+        1/2 second
+      </button>
+      <button
+        onClick={() => {
+          setDelay(1000);
+        }}
+      >
+        1 second
+      </button>
+      <button
+        onClick={() => {
+          setDelay(2000);
+        }}
+      >
+        2 second
+      </button>
+      <button
+        onClick={() => {
+          setDelay(4000);
+        }}
+      >
+        4 second
+      </button>
+      <input value={delay} onChange={handleDelayChange}></input>
+
+      <div>{console.log(props.playPauseMode)}</div>
+
+      <ManualDisplayComponent
+        count={counter}
+        playPauseMode={props.playPauseMode}
+        setCount={setCounter}
+      />
+
+      <FetchData displayNumberDetails={counter} />
+    </>
+  );
+}
+
+function ManualDisplayComponent(props) {
+  return (
+    <>
+      <div>test Div manual control {props.count}</div>
+      <button onClick={props.setCounter}>+</button>
+      <button onClick={props.setCounter}>-</button>
+    </>
+  );
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 export default AutomaticDisplayComponent;
